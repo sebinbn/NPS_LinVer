858ba0dc37d63b30e462ccb0857d779688ad5046
unknown <sebinbn@gmail.com>
2024-12-19 21:33:51 -0600

    Streamlined running LINVER for various types of analysis using varied for loops.

--- stochsims_NPS.m
+++ stochsims_NPS.m
@@ -13,8 +13,8 @@
 clear all;
 
 % Provide the path to Dynare (eg, addpath c:/dynare/5.0/matlab)
-addpath c:/dynare/5.2/matlab  % for Sebin's office laptop
-%addpath c:/dynare/5.4/matlab % for Sebin's personal laptop
+%addpath c:/dynare/5.2/matlab  % for Sebin's office laptop
+addpath c:/dynare/5.4/matlab % for Sebin's personal laptop
 
 % Supply model settings (mandatory unless otherwise indicated)
 %   rulevers -- Taylor 'T' or Balanced Approach  'BA' Rules
@@ -38,26 +38,35 @@ addpath c:/dynare/5.2/matlab  % for Sebin's office laptop
 
 inertial = 0; %0 - non-inertial rule or 1 - inertial rule 
 atit_tgts = [2.2, 2.25,2.5,3]; %asymmetric target for ATIT rule
-acit_coefs = [0.75,1.5]; %asymmetric coefficient for ACIT
+acit_coefs = [1,2]; %[0.75,1.5]; %asymmetric coefficient for ACIT
 aait_qtrs = [8,16,24]; %the number of qtrs for which avg inflation is calculated
- 
+hcit_coefs = [0.75,1,1.25]; %the higher coefficient in Trad.rule
+
 for expvers = "mcapwp" %["var","mceall","mcapwp","mcap"];
 for rulevers = ["T","BA"]
-%for mprule = ["intay","short","aait","acit","atit","ait"]
+% Any one For loop is to be activated amongst the following:
+% 1. Running various rules without variants of ACIT, ATIT, AAIT
+% 2. Running variants of ATIT with targets varying
+% 3. Running variants of ACIT with coefficients varying
+% 4. Running variants of AAIT with qtrs averaged varying
+% 5. Running variants of intay with higher coefficients (HCIT)
 
-%The following code is used to loop over multiple ACIT and ATIT rules using
-%coefficient and targets defined in acit_coefs and atit_tgts respectively
-%     for rep = 1:6
-%     if rep > 4
-%         mprule = "acit";
-%         acit_coef = acit_coefs(rep-4);
-%     else
-%         mprule = "atit";
-%         atit_tgt = atit_tgts(rep);
-%     end
-
-for aait_qtr = aait_qtrs
- mprule = "aait";
+%for mprule = ["intay","short","aait","acit","atit","ait"] %for running various rules without variants of ACIT, ATIT, AAIT
+    % acit_coef = 1; %default acit
+    % atit_tgt = 3; %default atit
+    % aait_qtr = 8; %default aait
+    %hcit_coef = 0.5; %default inflation coefficient
+%for atit_tgt = atit_tgts %loop over ATIT rule targets defined in atit_tgts
+%  mprule = "atit";
+%  hcit_coef = 0.5; %default inflation coefficient
+for acit_coef = 2%acit_coefs %loop over ACIT coefficients defined in acit_coefs
+  mprule = "acit"; 
+  hcit_coef = 0.5; %default inflation coefficient
+%for aait_qtr = aait_qtrs %for running AAIT with various qtr averages
+%  mprule = "aait";
+%  hcit_coef = 0.5; %default inflation coefficient
+%for hcit_coef = hcit_coefs %for running HCIT variants
+%  mprule = "intay";
 
 elb_imposed = "yes";
 elb = -3;  % required only if elb_imposed = "yes"
@@ -128,13 +137,15 @@ add_track_names = ["fiscal","rrff"];
 %alt_range = ["1983Q1","2019Q4"];
 %rescale_wpshocks = "no";
 save_option = "limited";
-save_name = 'saved_results/' + expvers + '_' + rulevers + '_' + mprule;
+save_name = "saved_results/" + expvers + "_" + rulevers + "_" + mprule;
 if mprule == "atit"
-    save_name = save_name + atit_tgt;
+    save_name = save_name + "_" + atit_tgt;
 elseif mprule == "acit"
-    save_name = save_name + acit_coef;
+    save_name = save_name + "_"+ acit_coef;
 elseif mprule == "aait"
-    save_name = save_name + aait_qtr;
+    save_name = save_name + "_"+ aait_qtr;
+elseif mprule == "intay"
+    save_name = save_name + "_" + hcit_coef;
 end
 save_name = save_name + '_results';
 % Verify the validity of user-selected settings and define various
@@ -185,7 +196,7 @@ stochloop;
 % Compute summary statistics and if selected, save results
 
 summarize_results_NPS;
-clearvars -except inertial expvers rulevers rep atit_tgts acit_coefs aait_qtrs
+clearvars -except inertial expvers rulevers rep atit_tgts acit_coefs aait_qtrs hcit_coefs
 
 end
 end
